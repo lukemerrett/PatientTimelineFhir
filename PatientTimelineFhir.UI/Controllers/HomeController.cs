f6e2612d7c10117741822f95c11f2ddf4d4caaf3
Luke Merrett <lukeamerrett@gmail.com>
2013-12-12 14:54:36 +0000

    Setup UI framework
    Added an MVC project for displaying a patients time line and set up the
    home controller to have the correct dependencies

---
+++ PatientTimelineFhir.UI/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using RestClientForFHIR.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PatientTimelineFhir.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPatientManager _patientManager;
+
+        private ITimelineManager _timelineManager;
+
+        public HomeController(IPatientManager patientManager, ITimelineManager timelineManager)
+        {
+            _patientManager = patientManager;
+            _timelineManager = timelineManager;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ PatientTimelineFhir.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PatientTimelineFhir.UI.MvcApplication" Language="C#" %>
