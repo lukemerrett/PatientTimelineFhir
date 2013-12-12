eac2a4c87d889a4b8173131cc7e5572cf2869b77
Luke Merrett <lukeamerrett@gmail.com>
2013-12-12 15:20:27 +0000

    Added single entry point for the App project
    There is now a single entry point into the Application project through
    the TimelineManager; which then co-ordinates the PatientManager and
    TimelineBuilder.  This keeps the UI nice and skinny

---
+++ Managers/ITimelineManager.cs
@@ -0,0 +1,14 @@
+﻿using RestClientForFHIR.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClientForFHIR.Managers
+{
+    public interface ITimelineManager
+    {
+        PatientTimeline GetTimelineForPatient(string givenName, string familyName);
+    }
+}
---
+++ Managers/TimelineManager.cs
@@ -0,0 +1,58 @@
+﻿using Hl7.Fhir.Client;
+using RestClientForFHIR.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClientForFHIR.Managers
+{
+    public class TimelineManager : ITimelineManager
+    {
+        private readonly IPatientManager _patientManager;
+
+        private readonly ITimelineBuilder _timelineBuilder;
+
+        public TimelineManager(string apiBaseUrl)
+        {
+            var client = new FhirClient(new Uri(apiBaseUrl));
+
+            _patientManager = new PatientManager(client);
+            _timelineBuilder = new TimelineBuilder(client);
+        }
+
+        public PatientTimeline GetTimelineForPatient(string givenName, string familyName)
+        {
+            var patients = _patientManager.GetPatientsByName("Roelof Olaf", "Bor");
+
+            if (!patients.Any())
+            {
+                throw new Exception("Could not find a matching patient");
+            }
+
+            if (patients.Count() > 1)
+            {
+                throw new Exception("More than one patient found matching this name.");
+            }
+
+            var patient = patients.Single();
+
+            var patientIdentifier = patient.Identifier.FirstOrDefault();
+
+            if (patientIdentifier == null)
+            {
+                throw new Exception("The patient does not have an associated identifier.");
+            }
+
+            var timeline = _timelineBuilder.GetTimelineForPatient(patientIdentifier.Key);
+
+            return new PatientTimeline
+            {
+                PatientGivenName = givenName,
+                PatientFamilyName = familyName,
+                Timeline = timeline
+            };
+        }
+    }
+}
